9cdaa96f63b3e4d03630a08e29f2e8aeb91ab1a9
rakutenjongseok <whdtjr2792@gmail.com>
2019-08-13 07:16:14 +0900

    [add] basic router

--- server/app.js
+++ server/app.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("API RUNNiNG ..."));
 
+// @@ Routes
+app.use("/api/users", require("./src/routes/api/users"));
+app.use("/api/auth", require("./src/routes/api/auth"));
+app.use("/api/profile", require("./src/routes/api/profile"));
+app.use("/api/posts", require("./src/routes/api/posts"));
+
 app.listen(PORT, () => {
   console.log(`Server is up on PORT ${PORT}`);
 });
